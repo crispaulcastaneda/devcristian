83dba8928d062c5e86f58376d1718ee0f6d48c35
Cristian Paul Castañeda <cristianpaul_c@yahoo.com>
2025-07-21 17:37:26 +0800

    V1.3 Edit content in project-data

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,19 +7,19 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: 'Frontend Dev: Maria Ozawa Website',
+    title: 'Frontend Dev | Maria Ozawa Website',
     year: 2022,
     description: 'iGaming Partnership Website',
     url: 'https://project1.clientbeta.tech/',
   },
   {
-    title: 'FullStack Dev: Guess The NBA Champion',
+    title: 'FullStack Dev | Guess The NBA Champion',
     year: 2025,
     description: 'Interactive landing page for a game',
     url: 'https://project5.clientbeta.tech/',
   },
   {
-    title: 'WordPress Dev: MSEG',
+    title: 'WordPress Dev | MSEG ',
     year: 2023,
     description: 'Mansion Sports & Entertainment Group',
     url: 'https://mansionsports.com/',
