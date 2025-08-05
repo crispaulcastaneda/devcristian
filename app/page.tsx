7a51a34054c1cf641fb24cc5612f8b0eb54634b1
Cristian Paul Castañeda <cristianpaul_c@yahoo.com>
2025-08-05 16:20:28 +0800

    Delete unused media

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ export default function Page() {
   return (
     <section>
       <Image
-        src="/tdc.png"
+        src="/photos/dp.jpeg"
         alt="Profile photo"
         className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
         unoptimized
-        width={160}
-        height={160}
+        width={150}
+        height={100}
         priority
       />
       <h1 className="mb-8 text-2xl font-medium">Hi, I'm Paul!</h1>
